73d4238dc55ca2a41a9da27b3222f82227477f86
madrosDK <danielkoglek@gmx.at>
2025-07-30 15:19:59 +0200

    Update module.php

--- IrrigationController/module.php
+++ IrrigationController/module.php
@@ -23,10 +23,52 @@ class IRRIrrigationController extends IPSModule
     public function ApplyChanges()
     {
         parent::ApplyChanges();
+        // Hier könnten Timer o.ä. gesetzt werden, je nach Modus
     }
 
     public function RequestAction($Ident, $Value)
     {
-        // Wird später ergänzt
+        // Hier kannst du später manuelle Aktionen (z. B. Ventil manuell starten) behandeln
+    }
+
+    private function ReadSensorValue(int $sensorID): ?float
+    {
+        if ($sensorID <= 0) {
+            return null;
+        }
+        $value = @GetValue($sensorID);
+        return is_numeric($value) ? floatval($value) : null;
+    }
+
+    private function ShouldWater(): bool
+    {
+        $sensor1 = $this->ReadSensorValue($this->ReadPropertyInteger('MoistureSensor1'));
+        $sensor2 = $this->ReadSensorValue($this->ReadPropertyInteger('MoistureSensor2'));
+
+        $threshold = $this->ReadPropertyInteger('MoistureThreshold');
+
+        $belowThreshold = [];
+        if (!is_null($sensor1)) $belowThreshold[] = $sensor1 < $threshold;
+        if (!is_null($sensor2)) $belowThreshold[] = $sensor2 < $threshold;
+
+        return in_array(true, $belowThreshold, true);
+    }
+
+    private function ActivateWatering()
+    {
+        $duration = $this->ReadPropertyInteger('Duration');
+        $valve1 = $this->ReadPropertyInteger('Valve1');
+        $valve2 = $this->ReadPropertyInteger('Valve2');
+        $pump = $this->ReadPropertyInteger('Pump');
+
+        if ($pump > 0) RequestAction($pump, true);
+        if ($valve1 > 0) RequestAction($valve1, true);
+        if ($valve2 > 0) RequestAction($valve2, true);
+
+        IPS_Sleep($duration * 60 * 1000);
+
+        if ($valve1 > 0) RequestAction($valve1, false);
+        if ($valve2 > 0) RequestAction($valve2, false);
+        if ($pump > 0) RequestAction($pump, false);
     }
 }
